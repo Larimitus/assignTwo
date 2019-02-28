a5305d3681b1befb8ec1a8bd703f0a5dcfbb9b86
Hilario Mendez <hmendezvallejo@unomaha.edu>
2019-02-28 15:15:07 -0600

    Customer List Editing and Crispy Forms

--- mfscrm/crm/urls.py
+++ mfscrm/crm/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 app_name = 'crm'
 urlpatterns = [
     path('customer_list', views.customer_list, name='customer_list'),
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
 ]
