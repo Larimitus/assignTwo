d761625d12feb03a1eaafebc8d0ee55f3f0911da
Hilario Mendez <hmendezvallejo@unomaha.edu>
2019-03-30 10:35:03 -0500

    Remove Venv, DB, Migrations

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\D33P_Cyphor\Documents\Computer Programming\WebDevApp\mavFoodServOrg\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
