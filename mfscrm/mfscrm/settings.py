bb447958e086230390edfe9b734d6b2e953a2906
Hilario Mendez <hmendezvallejo@unomaha.edu>
2019-03-30 09:18:10 -0500

    Web Settings

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$of14+44950pf$^dvazs=7+bi860v7i_0=o-bn2z-6e@%9bg0)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT = '/home'
 LOGOUT_REDIRECT_URL = '/home'
